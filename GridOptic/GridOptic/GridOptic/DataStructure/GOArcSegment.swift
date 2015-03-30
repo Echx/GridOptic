e9093dcc4f8119e2520d5494dff43b86162306c0
Wang Jinghan <phantom.there@gmail.com>
2015-03-30 17:34:14 +0800

    GOArcSegment

--- GridOptic/GridOptic/GridOptic/DataStructure/GOArcSegment.swift
+++ GridOptic/GridOptic/GridOptic/DataStructure/GOArcSegment.swift
@@ -9,5 +9,40 @@
 import UIKit
 
 class GOArcSegment: NSObject {
-   
+    var center: GOCoordinate
+    var radius: CGFloat
+    var radian: CGFloat
+    //angle from [0, PI)
+    var normal: CGVector
+    
+    init(center: GOCoordinate, radius: CGFloat, radian: CGFloat, normal: CGVector) {
+        self.center = center
+        self.radius = radius
+        self.radian = radian
+        self.normal = normal
+    }
+    
+    var startPoint: CGPoint {
+        get {
+            return CGPointZero
+        }
+    }
+    
+    var endPoint: CGPoint {
+        get {
+            return CGPointZero
+        }
+    }
+    
+    var startRadian: CGFloat {
+        get {
+            return 0.0
+        }
+    }
+    
+    var endRadian: CGFloat {
+        get {
+            return 0.0
+        }
+    }
 }
