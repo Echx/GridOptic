ee83c1a788d7982ac3c275e77d3df59118b5979d
Lei Mingyu <uygnim.iel@gmail.com>
2015-04-03 03:27:42 +0800

    lens update

--- GridOptic/GridOptic/GameTestViewController.swift
+++ GridOptic/GridOptic/GameTestViewController.swift
@@ -69,7 +69,7 @@ class GameTestViewController: UIViewController {
 //        self.grid?.addInstrument(flatLens)
         let concaveLens = GOConcaveLensRep(center: GOCoordinate(x: 20, y: 15), direction: CGVectorMake(1, 4), thicknessCenter: 1, thicknessEdge: 3, curvatureRadius: 5, id: "CONCAVE_LENS_1", refractionIndex: 0.8)
 //        self.grid?.addInstrument(concaveLens)
-        let convexLens = GOConvexLensRep(center: GOCoordinate(x: 44, y: 20), direction: CGVectorMake(-1, -1), thickness: 2, curvatureRadius: 30, id: "CONVEX_LENS_1", refractionIndex: 0.8)
+        let convexLens = GOConvexLensRep(center: GOCoordinate(x: 44, y: 25), direction: CGVectorMake(0, -1), thickness: 2, curvatureRadius: 3, id: "CONVEX_LENS_1", refractionIndex: 1.8)
 //        self.grid?.addInstrument(convexLens)
     }
     
