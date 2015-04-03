ec508edb2ab8f39018af59d3e4616095e53bc016
Wang Jinghan <phantom.there@gmail.com>
2015-04-03 23:05:03 +0800

    年

--- GridOptic/GridOptic/GridOptic/DataStructure/GOStack.swift
+++ GridOptic/GridOptic/GridOptic/DataStructure/GOStack.swift
@@ -2,8 +2,8 @@
 //  GOStack.swift
 //  GridOptic
 //
-//  Created by NULL on 02/04/15.
-//  Copyright (c) 2015年 Echx. All rights reserved.
+//  Created by Wang Jinghan on 02/04/15.
+//  Copyright (c) 2015 Echx. All rights reserved.
 //
 
 import UIKit
--- GridOptic/GridOptic/GridOptic/OpticRepresentation/GOFlatOpticRep.swift
+++ GridOptic/GridOptic/GridOptic/OpticRepresentation/GOFlatOpticRep.swift
@@ -2,8 +2,8 @@
 //  GOFlatOpticRep.swift
 //  GridOptic
 //
-//  Created by NULL on 01/04/15.
-//  Copyright (c) 2015年 Echx. All rights reserved.
+//  Created by Wang Jinghan on 01/04/15.
+//  Copyright (c) 2015 Echx. All rights reserved.
 //
 
 import UIKit
